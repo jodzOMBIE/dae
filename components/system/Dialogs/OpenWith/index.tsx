fe4b370e7e388df87c2edc55a08fcd2c35191bb2
jodzOMBIE <93054307+jodzOMBIE@users.noreply.github.com>
2025-12-22 03:15:41 +0100

    Add 'Browser' to INCLUDED_PROCESSES set

--- components/system/Dialogs/OpenWith/index.tsx
+++ components/system/Dialogs/OpenWith/index.tsx
@@ -13,6 +13,7 @@ import { PREVENT_SCROLL, TRANSITIONS_IN_MILLISECONDS } from "utils/constants";
 import { getExtension, haltEvent } from "utils/functions";
 
 const INCLUDED_PROCESSES = new Set([
+  "Browser",
   "BoxedWine",
   "JSDOS",
   "Marked",
